6764eab5c905f1d34fe7ebaea12b3a0d97cf4d3d
zhuwenlong <zhuwenlong@autohome.com.cn>
2022-01-19 17:24:25 +0800

    test完善

--- test/day.test.ts
+++ test/day.test.ts
@@ -5,7 +5,7 @@ describe('day', () => {
     const date = day('2022-01-19')
     expect(date.year()).toBe(2022)
     // month 是从 0 到 11 的
-    expect(date.month()).toBe(1)
+    expect(date.month()).toBe(0)
     expect(date.date()).toBe(19)
   })
 })
